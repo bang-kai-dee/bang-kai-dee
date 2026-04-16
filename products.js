7ac1bd2c5da5d92acd152d86b5c9d80bd1f2a851
bang kai dee <dfft791@gmail.com>
2026-04-16 21:00:08 +0700

    update

---
+++ products.js
@@ -0,0 +1,47 @@
+// products.js
+const LINE_LINK = "https://lin.ee/6lbDXKS"; // 
+
+const productsData = [
+    {
+        id: 1,
+        name: "ฟาเทค",
+        price: 590,
+        image: "/images/fa.jpg",
+        lineLink: `${LINE_LINK}`
+    },
+    {
+        id: 2,
+        name: "เสื้อยืด Oversize สีพื้น",
+        price: 250,
+        image: "https://placehold.co/500x500/eaeaea/999?text=Product+2",
+        lineLink: `${LINE_LINK}`
+    },
+    {
+        id: 3,
+        name: "หมวกแก๊ป วินเทจ",
+        price: 190,
+        image: "https://placehold.co/500x500/eaeaea/999?text=Product+3",
+        lineLink: `${LINE_LINK}`
+    },
+    {
+        id: 4,
+        name: "กระเป๋าสะพาย รุ่นมินิมอล",
+        price: 590,
+        image: "https://placehold.co/500x500/eaeaea/999?text=Product+4",
+        lineLink: `${LINE_LINK}`
+    },
+    {
+        id: 5,
+        name: "เสื้อยืด Oversize สีพื้น",
+        price: 250,
+        image: "https://placehold.co/500x500/eaeaea/999?text=Product+5",
+        lineLink: `${LINE_LINK}`
+    },
+    {
+        id: 6,
+        name: "หมวกแก๊ป วินเทจ",
+        price: 190,
+        image: "https://placehold.co/500x500/eaeaea/999?text=Product+6",
+        lineLink: `${LINE_LINK}`
+    },
+];
